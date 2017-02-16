6c3ba1eee9af814ccff1d40042a95dd721308e4c
Troy Negus <troynegus@gmail.com>
2017-02-16 15:37:38 +1300

    Added loading screen

---
+++ Assets/Scripts/Main Scripts/Loading.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loading : MonoBehaviour {
+    private static string level;
+
+    void Update()
+    {
+        SceneManager.LoadScene(level);
+    }
+	
+	public static void loadLevel(string lvl)
+    {
+        SceneManager.LoadScene("Loading");
+        level = lvl;
+    }
+}
