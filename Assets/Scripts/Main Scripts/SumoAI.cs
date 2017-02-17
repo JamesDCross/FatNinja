c1a73cdb1adfe5aba2f7ae5944c6f2ab821646ab
Reuben Crimp <reubencrimp@gmail.com>
2017-02-17 17:34:02 +1300

    boss qte phase 1

--- Assets/Scripts/Main Scripts/SumoAI.cs
+++ Assets/Scripts/Main Scripts/SumoAI.cs
@@ -222,7 +222,7 @@ public class SumoAI : MonoBehaviour
                 audioE.clip = painSounds[death];
                 audioE.Play();
 
-                Loading.loadLevel("finalQTE");
+                Loading.loadLevel("bossQTE");
             }
 
             //play audio
