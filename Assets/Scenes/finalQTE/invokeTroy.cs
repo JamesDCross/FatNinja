dc1146e310dadcab8a2116501db2619d74a592de
albertgao <albertgaohy@gmail.com>
2017-02-17 17:37:18 +1300

    add the InvokeTroy function

---
+++ Assets/Scenes/finalQTE/invokeTroy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+using InControl;
+
+[CommandInfo("QTE", "Invoke Troy loadLevel", "Use the method")]
+public class invokeTroy : Command
+{
+    public string sceneName;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    public override void OnEnter()
+    {
+        Loading.loadLevel(sceneName);
+    }
+
+    void Update()
+    {
+
+    }
+}
